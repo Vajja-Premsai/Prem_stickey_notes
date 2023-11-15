949236c94b29997b66b774f17738446cfc829602
Vajja Premsai <108271437+Vajja-Premsai@users.noreply.github.com>
2023-11-16 01:22:54 +0530

    Update MastHead.jsx

--- components/MastHead/MastHead.jsx
+++ components/MastHead/MastHead.jsx
@@ -2,9 +2,9 @@ export default function MastHead() {
     return (
         <header id="masthead" className="app-header">
             <h2>
-                <img src="https://premsaiv.in/portfolio/favicon.png" className="app-logo" alt="logo" />
+{/*                 <img src="https://premsaiv.in/portfolio/favicon.png" className="app-logo" alt="logo" /> */}
                 Personal Sticky notes||React||❤️
-                <img src="https://premsaiv.in/portfolio/favicon.png" className="app-logo" alt="logo" />
+{/*                 <img src="https://premsaiv.in/portfolio/favicon.png" className="app-logo" alt="logo" /> */}
             </h2>
         </header>
     )
